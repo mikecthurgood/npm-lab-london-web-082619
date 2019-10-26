298e680fdcc963b1ba31a6cf1dfc32dae563ddff
Mike Thurgood <mikecthurgood@gmail.com>
2019-10-26 13:52:13 +0100

    Done.

--- color-clock/index.js
+++ color-clock/index.js
@@ -3,11 +3,11 @@
 function displayTime() {
   let clock = document.querySelector('#clock')
   clock.innerText = moment().format('MMMM Do YYYY, h:mm:ss a')
-  setInterval(function() {
-      clock.innerText = moment().format('MMMM Do YYYY, h:mm:ss a')
-      clock.style.color = '#'+Math.floor(Math.random()*16777215).toString(16);
-      clock.style.backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
-  }, 1000)
+  setInterval(function () {
+    clock.innerText = moment().format('MMMM Do YYYY, h:mm:ss a')
+    clock.style.color = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    clock.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+  }, 500)
 
 
 }
