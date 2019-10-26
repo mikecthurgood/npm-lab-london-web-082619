ef1115068e1a3792ae633409ad65fd54bb5b9d11
Mike Thurgood <mikecthurgood@gmail.com>
2019-10-26 13:53:28 +0100

    Done.

--- color-clock/index.js
+++ color-clock/index.js
@@ -7,7 +7,7 @@ function displayTime() {
     clock.innerText = moment().format('MMMM Do YYYY, h:mm:ss a')
     clock.style.color = '#' + Math.floor(Math.random() * 16777215).toString(16);
     clock.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
-  }, 500)
+  }, 250)
 
 
 }
